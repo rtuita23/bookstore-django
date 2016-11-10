c5056da0cb5b0dd3270818cdeeec225f097891ed
yasmaR Tuita <rktuita23@hotmail.com>
2016-11-10 07:22:43 +0000

    created urls in app folder

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'store.views.index', name='index'),
-    url(r'^store', 'store.views.store', name='store')
+    url(r'^store', include('store.urls'), name='store')
 ]
